9c8a40f357fb355e8846acd23b95d8018e15774f
Mike Thompson <mfthomps@nps.edu>
2020-03-03 17:35:53 -0800

    share /dev if using thumb drive

--- gns3server/compute/docker/docker_vm.py
+++ gns3server/compute/docker/docker_vm.py
@@ -250,6 +250,9 @@ class DockerVM(BaseNode):
             log.debug('doing X11 binds for %s' % self._image)
             #volume = volume+' --env="DISPLAY"  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"'
             binds.append("/tmp/.X11-unix:/tmp/.X11-unix:rw")
+            if labtainersGNS3.hasThumb(self._image, log):
+                binds.append("/dev:/dev:rw")
+            
 
         self._volumes = ["/etc/network"]
         volumes = image_infos.get("Config", {}).get("Volumes")
